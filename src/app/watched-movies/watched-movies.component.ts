1e7d8280c227e538b9195c2926cf97e5640686dd
Zico Vanheuckelom <71635898+ZicoVH@users.noreply.github.com>
2022-11-23 19:59:55 +0100

    search watched works, needs optimalization!

--- src/app/watched-movies/watched-movies.component.ts
+++ src/app/watched-movies/watched-movies.component.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription, Observable, fromEvent, map, filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import { Movie } from '../movie';
 import { MovieService } from '../movie.service';
 
 @Component({
@@ -10,15 +13,54 @@ import { MovieService } from '../movie.service';
 })
 export class WatchedMoviesComponent implements OnInit {
   movies: any = [];
+  searchMovies: any = [];
+  showSearchResults: boolean = false;
+  opinion: boolean = false;
 
   constructor(private router: Router,private movieService: MovieService, private route: ActivatedRoute,private httpClient: HttpClient) { }
+
+  searchWatched = new FormGroup({
+    type: new FormControl(),
+    comment: new FormControl()
+  });
 
   ngOnInit(): void {
     this.getAllMoviesFromWatched();
 
-
   }
 
+
+
+  SearchWatched(submitted: boolean) {
+    this.searchMovies = []
+    if (this.searchWatched.value.type == "Comment" || !this.searchWatched.value.type ){
+      this.movieService.searchSpecificCommentDatabase(this.searchWatched.value.comment).subscribe((r:any) => {
+        for (var i=0; i < r.length; i++) {
+
+          let movieId = r[i].movieId;
+          this.movieService.getMovieById(movieId).subscribe((r:any)=> {
+            this.searchMovies.push(r);
+            console.log(r)
+          })
+          this.showSearchResults = !submitted;
+        }
+      })
+    }
+    if (this.searchWatched.value.type == "Rating" ){
+      this.movieService.searchSpecificRatingDatabase(this.searchWatched.value.comment).subscribe((r:any) => {
+        for (var i=0; i < r.length; i++) {
+
+          let movieId = r[i].movieId;
+          this.movieService.getMovieById(movieId).subscribe((r:any)=> {
+            this.searchMovies.push(r);
+            console.log(r)
+          })
+          this.showSearchResults = !submitted;
+        }
+      })
+    }
+
+  }
 
   getAllMoviesFromWatched() {
     this.movieService.getWatchedList().subscribe((r:any)=> {
@@ -39,5 +81,4 @@ export class WatchedMoviesComponent implements OnInit {
       }
     })
   }
-
 }
