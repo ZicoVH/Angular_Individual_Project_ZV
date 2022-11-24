7f1447c479b1bd5c1a7e512be5e5ab46842c2b58
Zico Vanheuckelom <71635898+ZicoVH@users.noreply.github.com>
2022-11-24 16:55:04 +0100

    Java Api added from outside Github

---
+++ java_api/src/main/java/fact/it/individualprojectjavazv/repository/MovieRepository.java
@@ -0,0 +1,17 @@
+package fact.it.individualprojectjavazv.repository;
+
+import fact.it.individualprojectjavazv.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+     Movie findByMovieId(int id);
+
+     @Query("select b from Movie b where b.watchedorNot is true ")
+     List<Movie> findAllWatched();
+
+
+
+}
